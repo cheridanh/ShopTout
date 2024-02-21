62834566439e1151fbf47646e62d94303c95c049
Chéridanh TSIELA <divintsiela@gmail.com>
2024-02-21 09:04:44 +0100

    adding AdminController and her views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::name('admin.')->prefix('admin')->group(function () {
+   Route::resource('home', AdminController::class);
 });
