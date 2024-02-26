b7853386e15db095b13b4f4a6e604a2cef82c12f
Chéridanh TSIELA <divintsiela@gmail.com>
2024-02-26 22:05:23 +0100

    updating view home

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -8,15 +8,18 @@
 
         @include('partials.search')
 
-        <section class="">
-            <div class="row row-cols-1 row-cols-md-4 g-4">
-                @foreach($articles as $article)
-                    <div class="col">
-                        @include('partials.card')
-                    </div>
-                @endforeach
-            </div>
-        </section>
+        <div class="row">
+            @foreach($articles as $article)
+                <div class="col-3 mb-4">
+                    @include('partials.card')
+                </div>
+            @endforeach
+        </div>
+
+        <div class="my-4">
+            {{ $articles->links() }}
+        </div>
+    </div>
 
     </div>
 
