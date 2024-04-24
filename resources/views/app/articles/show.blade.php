8b7998afbc29a96cdb33a06d1f000586ed022f2f
Chéridanh <divintsiela@gmail.com>
2024-04-24 16:35:33 +0200

    adding to show view a description for an article

--- resources/views/app/articles/show.blade.php
+++ resources/views/app/articles/show.blade.php
@@ -12,28 +12,7 @@
 
         <div class="row">
             <div class="col-4">
-                <div id="carouselExampleIndicators" class="carousel slide">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    <div class="carousel-inner">
-                        @foreach($article->pictures as $k => $picture)
-                            <div class="carousel-item {{ $k == 0 ? 'active' : '' }}">
-                                <img src="{{ $picture->getImageUrl(800, 530) }}" class="d-block w-100" alt="...">
-                            </div>
-                        @endforeach
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
+                @include('partials.carousel')
             </div>
             <div class="col-8">
                 <div class="mt-4">
@@ -61,34 +40,32 @@
                         </div>
                     </form>
                 </div>
-
-                <div class="mt-4">
-                    <h2>Description</h2>
-                    <p>{{ nl2br($article->description) }}</p>
-                    {{--
-                    <div class="row">
-                        <div class="col-4">
-                            <h2>Tailles disponibles</h2>
-                            <ul class="list-group">
-                                @foreach($article->sizes as $size)
-                                    <li class="list-group-item">
-                                        {{ $size->name }}
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        <div class="col-3">
-                            <h2>Couleurs</h2>
-                            <ul class="list-group">
-                                @foreach($article->colors as $color)
-                                    <li class="list-group-item">
-                                        {{ $color->name }}
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                    --}}
+                <hr>
+            </div>
+        </div>
+        <div class="mt-4">
+            <h2>Description</h2>
+            <p>{{ nl2br($article->description) }}</p>
+            <div class="row">
+                <div class="col-4">
+                    <h2>Tailles disponibles</h2>
+                    <ul class="list-group">
+                        @foreach($article->sizes as $size)
+                            <li class="list-group-item">
+                                {{ $size->name }}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="col-3">
+                    <h2>Couleurs</h2>
+                    <ul class="list-group">
+                        @foreach($article->colors as $color)
+                            <li class="list-group-item">
+                                {{ $color->name }}
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,17 +1,16 @@
-<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselPictures" class="carousel slide">
     <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="/assets/img/paris.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="/assets/img/beriz.jpg" class="d-block w-100" alt="...">
-        </div>
+        @foreach($article->pictures as $k => $picture)
+            <div class="carousel-item {{ $k == 0 ? 'active' : '' }}">
+                <img src="{{ $picture->getImageUrl(800, 530) }}" class="d-block w-100" alt="...">
+            </div>
+        @endforeach
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselPictures" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselPictures" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
